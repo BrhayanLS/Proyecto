42242947e1ccc3640e8d95d0b4811a6191e395da
BrhayanLS <brhayanls09@gmail.com>
2022-06-25 07:53:14 -0500

    se realiza ajustes modulo materia prima

---
+++ alertas.js
@@ -0,0 +1,32 @@
+function confirmar()
+{
+	Swal.fire({
+		title: '¿Seguro que desea ingresar:?',
+		icon: 'question',
+		showCancelButton: true,
+		confirmButtonColor: '#d33',
+		cancelButtonColor: '#6c757d',
+		confirmButtonText: 'Confirmar'
+	}).then((result) => {
+		/* Read more about isConfirmed, isDenied below */
+		if (result.isConfirmed) {
+			window.location='index.html';
+		}
+	})
+}
+
+function agregar()
+{
+	Swal.fire({
+		position: 'top-end',
+		icon: 'success',
+		title: 'Producto agregado!',
+		showConfirmButton: false,
+		timer: 1500
+	})
+}
+
+function registrar()
+{
+	Swal.fire('El pedido se ha registrado exitosamente!!')
+}
